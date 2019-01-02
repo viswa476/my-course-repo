a01617fe3bae1e50ea471b78a1572d40c51ada3e
Ubuntu <ubuntu@ip-172-31-30-116.ap-south-1.compute.internal>
2019-01-02 04:00:58 +0000

    PULLING pull.java File

--- pull.java
+++ pull.java
@@ -1,4 +1,14 @@
-fiel pulling
-kjvw
+file pulling
+jhasjfhiubbmzxz cc
+mklaHSIUFkb \	;d
+]kjBD mngsdm m j AASMgiubMAS DMbuibmn 
+pmnkjAHFI 
+JKBASKJF    
+MMNbajhsd   Nbaudydb mn
+t
+JKABSIJDNM 
+	 hjABH MBiubMN mkihwekj jkhk KJHSNDGM,NIOUN kjhkusdfbnmbiBMA
+BHJUABASHDM  JN
+
 kwbrv
 kjwvr
